041d2052319239347b307581d314a146fc1acb62
djangoprojectsot1 <74851474+djangoprojectsot1@users.noreply.github.com>
2021-06-24 00:39:38 +0530

    Update admin.py

--- csvproject/testapp/admin.py
+++ csvproject/testapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Contact,Contact1
-# Register your models here.
+# Register your models here. ai MANJBDFKJAS
 
 class ContactAdmin(admin.ModelAdmin):
     list_display=['name','email','message']
